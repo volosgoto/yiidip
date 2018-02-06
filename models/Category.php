4bd9aa454e6ee43dc4c87ef32f98e38f76f21d73
volosgoto <volosgoto@yahoo.com>
2018-02-06 09:58:32 +0200

    upd Product model

--- models/Category.php
+++ models/Category.php
@@ -15,10 +15,9 @@ class Category extends ActiveRecord {
     public static function tableName() {
         return 'category';
     }
-    
-    public function getProducts (){
-        return $this->hasMany(Product::className(), ['category_id']);
-    }
 
+    public function getProducts (){
+        return $this->hasMany(Product::className(), ['category_id' => 'id']);
+    }
 
 }
