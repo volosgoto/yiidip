15959195f897ee835082fbf19853db10e7bc0b11
volosgoto <volosgoto@yahoo.com>
2018-02-06 09:53:07 +0200

    add category and product models

---
+++ models/Category.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey
+ * Date: 06.02.18
+ * Time: 9:46
+ */
+
+namespace app\models;
+
+
+class Category {
+
+}
---
+++ models/Product.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey
+ * Date: 06.02.18
+ * Time: 9:47
+ */
+
+namespace app\models;
+
+
+class Product {
+
+}
