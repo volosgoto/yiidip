ff3750d92aa6f8656622ebec0811f19ac6a0b2aa
volosgoto <volosgoto@yahoo.com>
2018-02-06 10:15:08 +0200

    add MenuWidget

---
+++ components/MenuWidget.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrey
+ * Date: 06.02.18
+ * Time: 10:01
+ */
+
+namespace app\components;
+
+
+class MenuWidget {
+
+}
--- models/Product.php
+++ models/Product.php
@@ -10,7 +10,13 @@ namespace app\models;
 use yii\db\ActiveRecord;
 
 class Product extends ActiveRecord {
+
     public static function tableName() {
         return 'product';
+    }
+
+
+    public function getCategory (){
+        return $this->hasOne(Category::className(), ['id' => 'category_id']);
     }
 }
