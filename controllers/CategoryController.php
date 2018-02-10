c4c311b0190625797fbaf03b18ae13c5f8ad3b99
volosgoto <volosgoto@yahoo.com>
2018-02-10 15:53:40 +0200

    add metha tags

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -1,23 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: andrey
- * Date: 07.02.18
- * Time: 14:13
- */
 
 namespace app\controllers;
-
 use app\models\Category;
 use app\models\Product;
 use Yii;
 
-class CategoryController extends AppController {
-
+class CategoryController extends AppController{
 
     public function actionIndex(){
         $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
-        //debug($hits);
         return $this->render('index', compact('hits'));
     }
+
+    public function actionView($id){
+        $id = Yii::$app->request->get('id');
+        $products = Product::find()->where(['category_id' => $id])->all();
+        return $this->render('view', compact('products'));
+    }
+
 }
