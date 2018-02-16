4a7765b555adf59542c49671f13a0d4a66b36f56
volosgoto <volosgoto@yahoo.com>
2018-02-16 10:03:47 +0200

    add cart calear

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -26,4 +26,15 @@ class CartController extends AppController {
         $this->layout = true;
         return $this->render('cart-modal', compact('session'));
     }
+
+    public function actionClear(){
+        $session =Yii::$app->session;
+        $session->open();
+        $session->remove('cart');
+        $session->remove('cart.qty');
+        $session->remove('cart.sum');
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
+
 }
--- web/js/main.js
+++ web/js/main.js
@@ -7,6 +7,26 @@ $('.catalog').dcAccordion({
 	speed: 300
 });
 
+
+function showCart(cart){
+    $('#cart .modal-body').html(cart);
+    $('#cart').modal();
+}
+
+function clearCart(){
+    $.ajax({
+        url: '/cart/clear',
+        type: 'GET',
+        success: function(res){
+            if(!res) alert('Ошибка!');
+            showCart(res);
+        },
+        error: function(){
+            alert('Clear Error!');
+        }
+    });
+}
+
 $('.add-to-cart').on('click', function (e) {
     e.preventDefault();
     var id = $(this).data('id');
@@ -16,8 +36,8 @@ $('.add-to-cart').on('click', function (e) {
         type: 'GET',
         success: function(res){
             if(!res) {alert('Ошибка!');}
-            console.log(res);
-            //showCart(res);
+            // console.log(res);
+            showCart(res);
         },
         error: function(){
             alert('Error!');
