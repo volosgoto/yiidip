9bc6149d1e9e532d28f09d2144bde13ca7515b68
volosgoto <volosgoto@yahoo.com>
2018-02-16 14:32:14 +0200

    CartController

---
+++ controllers/CartController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Product;
+use app\models\Cart;
+use Yii;
+
+class CartController extends AppController {
+    public function actionAdd() {
+        $id = Yii::$app->request->get('id');
+        $product = Product::findOne($id);
+        if (empty($product)) return false;
+        $session = Yii::$app->session;
+        $session->open();
+        $cart = new Cart();
+        $cart->addToCart($product);
+        $this->layout = true;
+        return $this->render('cart-modal', compact('session'));
+    }
+
+    public function actionClear() {
+        $session = Yii::$app->session;
+        $session->open();
+        $session->remove('cart');
+        $session->remove('cart.qty');
+        $session->remove('cart.sum');
+        $this->layout = false;
+        return $this->render('cart-modal', compact('session'));
+    }
+}
