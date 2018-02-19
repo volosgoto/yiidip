3bc2aac473fcd187a5fac10198cd4ff285e65936
volosgoto <volosgoto@yahoo.com>
2018-02-19 10:46:14 +0200

    get Order

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -4,6 +4,8 @@ namespace app\controllers;
 
 use app\models\Product;
 use app\models\Cart;
+use app\models\Order;
+use app\models\OrderItems;
 use Yii;
 
 class CartController extends AppController {
@@ -53,7 +55,10 @@ class CartController extends AppController {
     }
 
     public function actionView(){
-        $this->layout = false;
-        return $this->render('view');
+        $session =Yii::$app->session;
+        $session->open();
+        $this->setMeta('Корзина');
+        $order = new Order();
+        return $this->render('view', compact('session', 'order'));
     }
 }
