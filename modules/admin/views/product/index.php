e77f067c18aeaab2952c4c4e47f661a15b394ea6
volosgoto <volosgoto@yahoo.com>
2018-02-28 10:01:49 +0200

    add admin\product

--- modules/admin/views/product/index.php
+++ modules/admin/views/product/index.php
@@ -6,15 +6,15 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Categories';
+$this->title = 'Products';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="category-index">
+<div class="product-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Category', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create Product', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
@@ -23,16 +23,16 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\SerialColumn'],
 
             'id',
-//            'parent_id',
-            [
-                'attribute' => 'parent_id',
-                'value' => function ($data){
-                    return $data->category ? $data->category->name : 'Самостоятельная категория';
-                }
-            ],
+            'category_id',
             'name',
-            'keywords',
-            'description',
+            'content:ntext',
+            'price',
+            //'keywords',
+            //'description',
+            //'img',
+            //'hit',
+            //'new',
+            //'sale',
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
