338674c4719589e5ebc263d887f8ab2e2ef991d4
volosgoto <volosgoto@yahoo.com>
2018-03-01 11:03:01 +0200

    add yii2-ckeditor

--- modules/admin/views/product/_form.php
+++ modules/admin/views/product/_form.php
@@ -1,6 +1,9 @@
 <?php
 
 use yii\helpers\Html;
+
+use mihaildev\ckeditor\CKEditor;
+//use mihaildev\elfinder\ElFinder;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
@@ -21,7 +24,14 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+    <?php
+    echo $form->field($model, 'content')->widget(CKEditor::className(), [
+        'editorOptions' => [
+            'preset' => 'full', //разработанны стандартные настройки basic, standard, full данную возможность не обязательно использовать
+            'inline' => false, //по умолчанию false
+        ],
+    ]);
+    ?>
 
     <?= $form->field($model, 'price')->textInput() ?>
 
@@ -31,12 +41,11 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'img')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'hit')->dropDownList([ '0', '1', ], ['prompt' => '']) ?>
+    <?= $form->field($model, 'hit')->checkbox([ '0', '1', ]) ?>
 
-    <?= $form->field($model, 'new')->dropDownList([ '0', '1', ], ['prompt' => '']) ?>
+    <?= $form->field($model, 'new')->checkbox([ '0', '1', ]) ?>
 
-    <?= $form->field($model, 'sale')->dropDownList([ '0', '1', ], ['prompt' => '']) ?>
-
+    <?= $form->field($model, 'sale')->checkbox([ '0', '1', ]) ?>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
