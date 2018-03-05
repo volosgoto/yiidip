a9086309628a1ac55e8fd88d395edfdcd5a219e1
volosgoto <volosgoto@yahoo.com>
2018-03-05 11:25:05 +0200

    upd update form

--- modules/admin/views/product/update.php
+++ modules/admin/views/product/update.php
@@ -5,11 +5,12 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\modules\admin\models\Product */
 
-$this->title = 'Update Product: {nameAttribute}';
+$this->title = 'Обновить продукт';
 $this->params['breadcrumbs'][] = ['label' => 'Products', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
+
 <div class="product-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
